b2955a549f3bf587a31fb9dc88e280d4528c3a76
Jade Jeffels <jadejeffels@hotmail.com>
2024-10-04 17:21:55 +0100

    added a score counting

--- RockPaperScissor/RPS.py
+++ RockPaperScissor/RPS.py
@@ -7,6 +7,7 @@ import sys
 
 print("Let's Play Rock, Paper, Scissors!\nThe rules are as follows:\nRock beats Scissors\nPaper beats Rock\nScissors beats Paper\nWhen you want to leave type exit")
 i = True
+computer = ""
 compScore = 0
 userScore = 0
 while(i == True):
@@ -33,20 +34,29 @@ while(i == True):
             compScore=compScore+1
         else:                               #rock vs scissor
             print("You Win")
+            userScore=userScore+1
     elif user == "paper" or user == "Paper":#user vs comp
         if computerNum == 1:                #Paper vs rock
             print("You Win")
+            userScore=userScore+1
         elif computerNum == 2:              #Paper vs paper
             print("Draw")
         else:                               #Paper vs scissor
             print("You Lose")
+            compScore=compScore+1
     elif user == "scissors" or user == "Scissors" or user == "scissor" or user == "Scissor":  
         if computerNum == 1:                #Scissor vs rock
             print("You Lose")
+            compScore=compScore+1
         elif computerNum == 2:              #Scissor vs paper
             print("You Win")
+            userScore=userScore+1
         else:                               #Scissor vs scissor
             print("Draw")
+    else:
+        print("Error Input was not expected. Please try again")
+    print(f"Current Score:\nUser:{userScore}   Computer:{compScore}")
+    
 
 
 
