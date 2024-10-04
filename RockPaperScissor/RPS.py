a01fe1534e480d88498fee4ccc9c3a815e9b8d37
Jade Jeffels <jadejeffels@hotmail.com>
2024-10-04 17:34:18 +0100

    Added comments

--- RockPaperScissor/RPS.py
+++ RockPaperScissor/RPS.py
@@ -6,15 +6,22 @@ import random
 import sys
 import tkinter
 
+#The game is in the function
 def RockPaperScissors():
     print("Let's Play Rock, Paper, Scissors!\nThe rules are as follows:\nRock beats Scissors\nPaper beats Rock\nScissors beats Paper\nWhen you want to leave type exit")
+    #Initialising some variables used in the loop
     i = True
     computer = ""
     compScore = 0
     userScore = 0
+
+    #Main loop of game
     while(i == True):
+        #Get the user input first
         user = input("3. 2. 1.\nUser:")
+
         #1 will be rock. 2 will be paper. 3 will be scissors
+        #Generates Computer Answer
         computerNum = random.randint(1,3)
         if computerNum == 1:
             computer == "Rock"
@@ -25,8 +32,9 @@ def RockPaperScissors():
 
         print("Computer: "+ computer)
 
+        #Decides the winner based on rules
         if user == "exit":
-            #close the game
+            #close the game     ###THIS DOESN'T CURRENTLY WORK###
             sys.exit
         elif user == "rock" or user == "Rock":  #user vs comp
             if computerNum == 1:                #rock vs rock
@@ -55,8 +63,10 @@ def RockPaperScissors():
                 userScore=userScore+1
             else:                               #Scissor vs scissor
                 print("Draw")
-        else:
+        #Anything other than the expected word answers
+        else:   
             print("Error Input was not expected. Please try again")
+        #Prints the score for the user to see the score
         print(f"Current Score:\nUser:{userScore}   Computer:{compScore}")
     
 
